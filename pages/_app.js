ae56ada61bb26753c5aeb63719b28eb8b1c9217a
Levi Harrison <levisamuelharrison@gmail.com>
2020-12-17 11:39:50 -0500

    Added Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import { usePanelbear } from '../hooks/panelbear'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+export default function Site({ Component, pageProps }) {
+  usePanelbear(process.env.NEXT_PUBLIC_ANALYTICS_ID)
+
   return <Component {...pageProps} />
 }
-
-export default MyApp
