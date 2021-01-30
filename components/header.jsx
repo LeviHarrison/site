6104320b93986e50195ce95fbdaccd32226cd260
Levi Harrison <levisamuelharrison@gmail.com>
2021-01-29 22:43:23 -0500

    Moved header into it's own component

---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from '../styles/Blog.module.css'
+
+export default function Header() {
+	return (
+		<Link href="/">
+        <a>
+          <h1 className={styles.home}>Levi Harrison</h1>
+        </a>
+    </Link>
+	)
+}
