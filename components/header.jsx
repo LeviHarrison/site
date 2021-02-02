4c250160d676617ea815dddfedf2368b17e824cc
Levi Harrison <levisamuelharrison@gmail.com>
2021-02-01 20:27:41 -0500

    Fixed missing stylesheet issue

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import styles from '../styles/Blog.module.css'
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
-	return (
-		<Link href="/">
-        <a>
-          <h1 className={styles.home}>Levi Harrison</h1>
-        </a>
+  return (
+    <Link href="/">
+      <a>
+        <h1 className={styles.home}>Levi Harrison</h1>
+      </a>
     </Link>
-	)
+  );
 }
