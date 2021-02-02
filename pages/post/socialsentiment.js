f47cda9ae7a2cdb8019f572b044728b99ebe0a91
Levi Harrison <levisamuelharrison@gmail.com>
2021-02-01 20:27:57 -0500

    Added Social Sentiment blog post

---
+++ pages/post/socialsentiment.js
@@ -0,0 +1,23 @@
+import Header from "../../components/header.jsx";
+import styles from "../../styles/Post.module.css";
+
+export default function Post() {
+  return (
+    <>
+      <Header />
+      <h1 className={styles.main}>Grüvee</h1>
+      <img className={styles.image} src="/socialsentiment-graph.png" />
+      <p className={styles.text}>
+        Over the summer, I wanted to try out AI, and with that came I cool
+        project idea. I decided to create a program that would find stock
+        tickers of different companies in Reddit posts, and then track the
+        sentiment in that post using Natural Language Processing. Separately, it
+        was also my first experience building microservices, which is an
+        architecture where different components are split off into separate
+        services. Pictured is the dashboard for Apple, which you can see was
+        tracking up at that time.
+      </p>
+      <img className={styles.image} src="/socialsentiment-terminal.png" />
+    </>
+  );
+}
