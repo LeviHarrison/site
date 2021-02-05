56d5745137d9548604e47ba03a6f25a277a5c579
Levi Harrison <54278938+LeviHarrison@users.noreply.github.com>
2021-02-05 17:56:54 +0000

    Rolled back events

--- components/repo.jsx
+++ components/repo.jsx
@@ -1,19 +1,19 @@
 import styles from '../styles/Repo.module.css'
 
-const Repo = ({ click, data }) => {
+const Repo = ({ data }) => {
 	return (
-				<a onClick={click(data.name)} href={`https://github.com/${data.owner.login}/${data.name}`} target="_blank">
+	    <a href={`https://github.com/${data.owner.login}/${data.name}`} target="_blank">
         	<div className={styles.repo}>
-						<h1>{data.name}</h1>
-            <h2>{data.owner.login}</h2>
-            <p>{data.description}</p>
-            <ul className={styles.languages}>
-                {data.languages.nodes.map((node, i) => <li className={styles.language} style={{ background: node.color }} key={i}>
-                    {node.name}
-                </li> )}
-            </ul>
-					</div>
-				</a>
+				<h1>{data.name}</h1>
+                <h2>{data.owner.login}</h2>
+                <p>{data.description}</p>
+                <ul className={styles.languages}>
+                    {data.languages.nodes.map((node, i) => <li className={styles.language} style={{ background: node.color }} key={i}>
+                        {node.name}
+                    </li> )}
+                </ul>
+		    </div>
+		</a>
     )
 }
 
--- components/repos.jsx
+++ components/repos.jsx
@@ -1,12 +1,5 @@
-import * as Panelbear from "@panelbear/panelbear-js"
 import styles from "../styles/Home.module.css"
 import Repo from "./repo"
-
-const click = async (name) => {
-    Panelbear.load(process.env.NEXT_PUBLIC_ANALYTICS_ID, {})
-
-    Panelbear.track("repo", name)
-}
 
 const Repos = ({ pinned }) => {
     return (
@@ -14,7 +7,7 @@ const Repos = ({ pinned }) => {
 		    <h1 className={styles.subheader}>Open Source Work</h1>
             <div className={styles.repos}>
                 {pinned.nodes.map((node, i) => (
-                    <Repo click={click} data={node} key={i} />
+                    <Repo data={node} key={i} />
                 ))}
             </div>
         </div>
